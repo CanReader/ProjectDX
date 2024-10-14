46f551c0bb31a75c0831f9c0dfc7cf798e45b56c
CanReader <canberk_nurcan@hotmail.com>
2024-10-15 02:56:42 +0300

    Add project files.

---
+++ Engine/src/foo.cpp
@@ -0,0 +1,6 @@
+#include "../include/foo.h"
+
+void foo() {
+	std::cout << "Hello DirectX!\n";
+	std::cin.get();
+}
---
+++ ProjectDX/main.cpp
@@ -0,0 +1,9 @@
+
+void foo();
+
+int main(int argc, char** argv) {
+
+	foo();
+
+	return 1;
+}
